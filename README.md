897edf5c57b06cf7909833e4a8c36346291ddd5f
egglou <miguel.rafaellouie2003@gmail.com>
2024-06-12 16:28:59 +0800

    init server and client

---
+++ README.md
@@ -0,0 +1,20 @@
+## REACT + EXPRESS + PRISMA (MYSQL)
+
+### 1. Install Dependencies
+- This is a monorepo project, so you need to install dependencies in both client and server folders.
+- For client folder
+```bash
+    cd client
+    npm install
+```
+- Root folder
+```bash
+    npm install
+```
+
+### 2. Start Development
+```bash
+npm run dev
+```
+Note: Should be in the root folder we will run concurrently the client and server.
+
---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+import express, {Express, Request, Response} from 'express';
+import cors from 'cors';
+
+const app: Express =express()
+const PORT = 5000
+app.use(cors())
+
+app.get('/', async (req: Request, res: Response) => {
+    res.json({ message: "Server is running"})
+})
+
+function logServerDetails(port: number) {
+    console.log(`\nServer is running`)
+    console.log(`\x1b[36m%s\x1b[0m`, `➜  Local:   http://localhost:${port}/`);
+}
+
+app.listen(PORT, () => {
+    logServerDetails(PORT)
+})
