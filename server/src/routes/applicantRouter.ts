b564775298e12190736abb517a1806bc3fc0c821
egglou <miguel.rafaellouie2003@gmail.com>
2024-06-13 07:25:07 +0800

    prettier format

--- server/src/routes/applicantRouter.ts
+++ server/src/routes/applicantRouter.ts
@@ -4,11 +4,13 @@ import { Applicant, CreateApplicantModel, UpdateApplicantModel } from '../models
 import { StatusCodes } from 'http-status-codes';
 
 export const applicantRouter = Router()
-    .get('/getApplicants', async(req:Request, res: Response) => {
+    .get('/getApplicants', async (req: Request, res: Response) => {
         const applicants: Applicant[] = [];
-        res.json({ message: 'Applicants retrieved successfully', applicants: applicants }).status(StatusCodes.OK);
+        res.json({ message: 'Applicants retrieved successfully', applicants: applicants }).status(
+            StatusCodes.OK
+        );
     })
-    .get('/getApplicant/:id', async(req: Request, res: Response) => {
+    .get('/getApplicant/:id', async (req: Request, res: Response) => {
         const applicant: Applicant = {
             address: '',
             applicantId: 0,
@@ -20,21 +22,29 @@ export const applicantRouter = Router()
             phoneNumber: '',
             references: [],
             sssNumber: ''
-        }
-        res.json({message: 'Applicant retrieved successfully', applicant: applicant}).status(StatusCodes.OK)
+        };
+        res.json({ message: 'Applicant retrieved successfully', applicant: applicant }).status(
+            StatusCodes.OK
+        );
     })
     .post(
-    '/createApplicant',
-    validateData(CreateApplicantModel),
-    async (req: Request, res: Response) => {
-        res.json({ message: 'Applicant created successfully', applicant: req.body }).status(
-            StatusCodes.CREATED
-        );
-    }
-)
-    .put('/updateApplicant/:id', validateData(UpdateApplicantModel), async(req: Request, res: Response) => {
-        res.json({ message: 'Applicant updated successfully', applicant: req.body}).status(StatusCodes.OK);
-    })
-    .delete('/deleteApplicant/:id', async(req: Request, res: Response) => {
-        res.json({ message: 'Applicant deleted successfully'}).status(StatusCodes.OK);
+        '/createApplicant',
+        validateData(CreateApplicantModel),
+        async (req: Request, res: Response) => {
+            res.json({ message: 'Applicant created successfully', applicant: req.body }).status(
+                StatusCodes.CREATED
+            );
+        }
+    )
+    .put(
+        '/updateApplicant/:id',
+        validateData(UpdateApplicantModel),
+        async (req: Request, res: Response) => {
+            res.json({ message: 'Applicant updated successfully', applicant: req.body }).status(
+                StatusCodes.OK
+            );
+        }
+    )
+    .delete('/deleteApplicant/:id', async (req: Request, res: Response) => {
+        res.json({ message: 'Applicant deleted successfully' }).status(StatusCodes.OK);
     });
